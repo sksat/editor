6f7adfbd28674117ebfad5321f0d7747bac6ce92
sksat <sksat.tec@gmail.com>
2018-04-03 19:59:59 +0900

    [UPDATE]

--- Makefile
+++ Makefile
@@ -6,8 +6,20 @@ LDFLAGS	=
 TARGET	= editor
 OBJS	= main.o
 
+RUNFLAGS=test.txt
+
 %.o:%cc
 	$(CXX) $(CXXFLAGS) $< -o $@
+
+default:
+	make $(TARGET)
+
+run:
+	./$(TARGET) $(RUNFLAGS)
+
+clean:
+	rm $(TARGET)
+	rm $(OBJS)
 
 $(TARGET):$(OBJS)
 	$(CXX) $(LDFLAGS) $^ -o $@
--- main.cc
+++ main.cc
@@ -1,7 +1,16 @@
-#include <sksat/cmdline.hpp>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+namespace editor {
+	void err(const std::string &str){
+		std::cerr<<str<<std::endl;
+		exit(-1);
+	}
+}
 
 int main(int argc, char **argv){
-	sksat::optparse op;
-	op.parse(argc, argv);
+	using std::string;
+	if(argc > 2) editor::err("too many argument.");
 	return 0;
 }
